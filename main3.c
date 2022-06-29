955c78fde6793777e7baefd198062861e31f7d8e
Eduardo Ribeiro <eduardorr.ws@gmail.com>
2022-06-29 01:30:25 -0300

    cores

--- main3.c
+++ main3.c
@@ -5,6 +5,8 @@
 #include <dirent.h>
 #include <time.h>
 
+
+
 char *  itoa( int value, char * str )
 {
     char temp;
@@ -33,7 +35,49 @@ char *  itoa( int value, char * str )
     return str;
 }
 
+void saindo(int led){
+    system("clear");
+    char *u = calloc(7, sizeof(char));
+    char *name = calloc(30, sizeof(char));
+    if(led==0) name = "SAINDO";
+    else name = "LOGANDO";
+
+    for(int i=0; i<3; i++){
+        if(i==0) u = ".";
+        if(i==1) u="..";
+        if(i==2) u="...";
+        //if(i==3) u="....";
+        //if(i==4) u=".....";
+        printf("\033[1;32m%s%s\033[m\n", name, u);
+        
+        sleep(1);
+        system("clear");
+    }
+}
+
+char *cores(int n){
+    char *cor = calloc(20, sizeof(char));
+    char *azul = calloc(20, sizeof(char));
+    char *semcor = calloc(20, sizeof(char));
+    char *branco = calloc(20, sizeof(char));
+    char *vermelho = calloc(20, sizeof(char));
+    semcor = "\033[m";
+    azul = "\033[1;36m";
+    branco = "\033[1;30m";
+    vermelho = "\033[1;31m";
+    if(n == 0) cor = semcor;
+    if(n == 1) cor = azul;
+    if(n == 2) cor = branco;
+    if(n == 3) cor = vermelho;
+
+    return cor;
+}
+
 void telalogin(){
+    
+    
+    //char *cor = calloc(20, sizeof(char));
+    
     int resp = -1, led = 0;
     char inp[16];
     FILE *arq;
@@ -45,13 +89,17 @@ void telalogin(){
     inp[0] = '-1';
     while(inp[0] != '0'){
         
-        printf("GAME TYPING\n\n"
-           "[ 1 ] Entrar\n"
-           "[ 2 ] Cadastrar\n\n"
-           "[ 0 ] Sair\n\n> ");
+        printf(
+            "%s===========================\n\n"
+            "  ----- TYPING GAME -----   \n\n"
+            "===========================%s\n\n"
+            "%s[ 1 ] Entrar\n"
+            "[ 2 ] Cadastrar\n\n"
+            "[ 0 ] Sair%s\n\n%s> %s", cores(1), cores(0), cores(2), cores(0), cores(3), cores(0));
         
         fgets(inp, 16, stdin);
         if(inp[0] == '0'){
+            saindo(0);
             exit(0);
         }
         resp = strtoull(inp, NULL, 16);
@@ -65,7 +113,7 @@ void telalogin(){
         switch (resp)
         {
         case 1:
-            printf("Nome: ");
+            printf("%sNome: %s", cores(1), cores(0));
             fgets(nome, 127, stdin);
             //printf("\nSenha: ");
             //fgets(senha, 29, stdin);
@@ -75,8 +123,8 @@ void telalogin(){
                 fgets(nome1, 127, arq);
                 if(strcmp(nome1, nome) == 0){
                     led = 1;
-                    system("clear");
-                    printf("LOGANDO...\n");
+                    
+                    saindo(1);
                     break;
                 }
             }
@@ -91,7 +139,7 @@ void telalogin(){
             break;
         
         case 2:
-            printf("\nNome Completo: ");
+            printf("CADASTRO\n\nNome Completo: ");
             fgets(nome, 127, stdin);
             arq = fopen("./logins/login.txt", "a+");
             while(!feof(arq)){
@@ -104,8 +152,10 @@ void telalogin(){
                 
             }
             if(led == 0){
-                fprintf(arq, "%s\n", nome);
-                printf("\nConta criada com sucesso!!\n");
+                fprintf(arq, "%s", nome);
+                sleep(2);
+                system("clear");
+                printf("Conta criada com sucesso!!\n");
                 
 
             }
@@ -128,20 +178,27 @@ void telamenu(){
     int resp = -1;
     char inp[16];
     FILE *arq;
-    system("clear");
+    
     inp[0] = '-1';
     while(inp[0] != '0'){
         system("clear");
         
-        printf("MENU\n\n"
-            "[ 1 ] Partida aleatoria\n"
+        printf(
+            "%s===========================\n\n"
+            "  ----- TYPING GAME ----- \n\n"
+            "===========================%s\n\n"
+            "%s[ 1 ] Partida aleatoria\n"
             "[ 2 ] Adicionar texto\n"
-            "[ 3 ] Estatisticas\n\n"
-            "[ 0 ] Sair do programa\n\n> ");
+            "[ 3 ] Ranking\n\n"
+            "[ 0 ] Sair%s\n\n%s> %s", cores(1), cores(0), cores(2), cores(0), cores(3), cores(0));
         
         fgets(inp, 16, stdin);
         resp = strtoull(inp, NULL, 16);
         
+        if(inp[0] == '0'){
+            saindo(0);
+            exit(0);
+        }
       
        
 
@@ -185,7 +242,7 @@ void telamenu(){
                 arq = fopen(texto, "r");
                     
                 partida(arq, textoarq);
-                printf("\nDigite qualquer tecla para continuar\n> ");
+                printf("\n%sDigite qualquer tecla para continuar%s\n\n%s> %s", cores(1), cores(0), cores(3), cores(0));
                 fgets(resp3, 5, stdin);
                 
 
@@ -216,8 +273,9 @@ void telamenu(){
                 strcat(texto, temp);
                 strcat(texto, ".txt");
                     
-                arq = fopen(texto, "w");
-                printf("Para adicionar um texto voce devera inserir linha por linha.\n\n");
+                //arq = fopen(texto, "w");
+                printf("%s===========================================================\n"
+                    "Para adicionar um texto voce devera inserir linha por linha%s\n\n", cores(1), cores(0));
                 int cont2 = 1;
                 char *resp2 = malloc(5 * sizeof(char));
                 
@@ -225,23 +283,25 @@ void telamenu(){
 
                 while(strcmp(resp2, "0") != 0){
                     char *textoarq2 = calloc(1024, sizeof(char));
-                    printf("Insira aqui a %d linha: ", cont2);
+                    printf("Insira aqui a %d linha: ", cont2
+                    );
                     fgets(textoarq2, 1024, stdin);
-                    strncat(textoarq, textoarq2, sizeof(textoarq2)-1);
+                    strcat(textoarq, textoarq2);
                     
                     
                     printf("\n");
                     printf("Digite qualquer tecla para continuar ou 0 para finalizar\n> ");
                     fgets(resp2, 5, stdin);
                     
-                    if(strcmp(resp2, "0\n")==0 && cont2 > 1){
+                    if(strcmp(resp2, "0\n")==0 && cont2 >= 3){
+                        arq = fopen(texto, "w");
                         fprintf(arq, "%s", textoarq);
                         break;
                     }
                     else{
-                        if(strcmp(resp2, "0\n")==0 && cont2 == 1){
+                        if(strcmp(resp2, "0\n")==0 && cont2 < 3){
                             system("clear");
-                            printf("Nao eh permitido adicionar apenas uma linha.\n");
+                            printf("%sNecessita-se ter ao menos 3 linhas%s\n", cores(3), cores(0));
                             sleep(2);
                             led =1;
                             break;
@@ -253,10 +313,17 @@ void telamenu(){
 
                     
                 }
-                sleep(2);
+                
                 system("clear");
                 
-                if(led == 0) printf("\nTexto adicionado com sucesso!\n");
+                if(led == 0){
+                    printf("%sTexto adicionado com sucesso!%s\n", cores(1), cores(0));
+                    fclose(arq);
+                    free(textoarq);
+                }
+
+                
+                
                 
          
             
@@ -266,7 +333,7 @@ void telamenu(){
             }
             sleep(3);
             system("clear");
-            fclose(arq);
+            
             closedir(dp);
             
             break;
@@ -285,13 +352,18 @@ void partida(FILE *arq, char *textoarq){
     time_t time_ini, time_fim;
     float tempo, vel;
     char *txt = calloc(1024, sizeof(char));
+    char *repetida = calloc(1024, sizeof(char));
     int cont, palavras=0;
     time_ini = time(NULL);
     int vidas = 3, led=0;
+    int score;
     while(!feof(arq)){
         system("clear");
-        
-        printf("VIDAS: ");
+        fgets(textoarq, 1024, arq);
+        if(strcmp(textoarq, repetida)==0){
+            break;
+        }
+        printf("%sVIDAS: %s", cores(1), cores(0));
         for(int i=vidas; i>0; i--){
             printf("\U0001F499 ");
         }
@@ -299,7 +371,8 @@ void partida(FILE *arq, char *textoarq){
 
         
         cont = 0;
-        fgets(textoarq, 1024, arq);
+        
+
         for(int i=0; textoarq[i]!='\0'; i++){
             if(isspace(textoarq[i]) != 0){
                 cont++;
@@ -311,30 +384,41 @@ void partida(FILE *arq, char *textoarq){
         if(strcasecmp(textoarq, txt) != 0){
             if(vidas == 1){
                 system("clear");
-                printf("FIM DE JOGO!\n\nVoce ficou sem vidas\n");
+                printf("%sFIM DE JOGO!\n\nVoce ficou sem vidas%s\n", cores(3), cores(0));
                 led =1;
                 sleep(4);
                 break;
             }
             vidas = vidas - 1;
         }
+        strcpy(repetida, textoarq);
     }
+    time_fim = time(NULL);
 
     if(led == 0){
-        printf("VIDAS: ");
+        system("clear");
+        printf("%sVIDAS: %s", cores(1), cores(0));
         for(int i=vidas; i>0; i--){
             printf("\U0001F499 ");
         }
         printf("\n\n");
-        time_fim = time(NULL);
+        printf("================================\n");
         tempo = difftime(time_fim, time_ini);
         tempo = tempo/60;
-        palavras += 1;
-        system("clear");
-        printf("%.2f minutos\n", tempo);
-        printf("%d palavras\n", palavras);
+        //palavras += 1;
+        
         vel = palavras / tempo;
-        printf("%.2f WPM (palavras por minuto)\n", vel);
+        if(vidas == 3) score = (((vel*vidas)*3 + palavras*3)+2)/(2);
+        if(vidas==2) score = (((vel*vidas)*2 + palavras)+2)/(2);
+        if(vidas==1) score = (((vel*vidas)/2 + palavras/2)+4)/(4);
+        printf("\033[1;32m           SCORE: %d\033[m\n", (int)score);
+        printf(
+            "\n%s%.2f minutos\n"
+            "%d palavras\n"
+            "%.2f WPM (palavras por minuto)%s\n", cores(2), tempo, palavras, vel, cores(0)
+        );
+        
+        printf("\n================================\n");
     }
     
 
